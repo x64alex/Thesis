cd320cfb1d3a92e91ee7dfdca21755c617868e3e
Cantor Alexandru <alexcantor64@gmail.com>
2024-06-07 22:57:24 +0300

    chore: build and push

--- backend.py
+++ backend.py
@@ -16,11 +16,11 @@ def get_response():
     data = request.json
     user_input = data.get('user_input')
     response = jcml.get_response(user_input)
-    return jsonify({'response': response}), 200, {'Access-Control-Allow-Origin': 'http://localhost:3000'}
+    return jsonify({'response': response}), 200, {'Access-Control-Allow-Origin': 'https://investmentchatbot.netlify.app'}
 
 @app.route('/update_json', methods=['POST'])
 def update_json():
     data = request.json
     jcml.update_configuration(data)
-    return jsonify({'message': 'JSON file updated successfully'}), 200, {'Access-Control-Allow-Origin': 'http://localhost:3000'}
+    return jsonify({'message': 'JSON file updated successfully'}), 200, {'Access-Control-Allow-Origin': 'https://investmentchatbot.netlify.app/'}
 
