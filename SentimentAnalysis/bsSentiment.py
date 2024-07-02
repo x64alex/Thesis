a0d29cffaed115c5953f7ea97531c78c3f81eb41
Cantor Alexandru <alexcantor64@gmail.com>
2024-07-02 17:18:39 +0300

    Update bsSentiment.py

--- SentimentAnalysis/bsSentiment.py
+++ SentimentAnalysis/bsSentiment.py
@@ -9,15 +9,15 @@ def bsSentiment(companyName):
     posts = client.get_posts(companyName, 20)
 
     sentiment = 0
-    
-    for i in range(30):
+
+    for i in range(10):
         sentiment_post = 0
         for post in posts:
             sentiment_post += sentiment_analysis_with_stochastic_model(post)    
 
         sentiment += sentiment_post/len(posts)
 
-    sentiment = sentiment /30
+    sentiment = sentiment /3
     sentiment *= 10
     if sentiment >=0:
         return f"{sentiment:.2f}% positive"
